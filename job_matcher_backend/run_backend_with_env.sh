929eb422f1ea0c621b4b6b8d19f14df8847f9258
sebibitica <biticasebi@gmail.com>
2025-06-05 18:21:44 +0300

    update backend container to wait for elasticsearch

--- job_matcher_backend/run_backend_with_env.sh
+++ job_matcher_backend/run_backend_with_env.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 printenv | grep -v "no_proxy" > /etc/container_environment
+
+# Wait for Elasticsearch
+echo "Waiting for Elasticsearch..."
+until curl -s http://elasticsearch:9200 | grep -q '"tagline" : "You Know, for Search"'; do
+  echo "Elasticsearch not ready. Retrying..."
+  sleep 2
+done
+
+echo "Elasticsearch is ready!"
+
 service cron start
 uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4
